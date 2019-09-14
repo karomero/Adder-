a179716949cf90e474581a0534234bc3f0fc84b6
karenr.romero8 <karenr.romero8@906-27.bmcc.cuny.edu>
2019-09-14 14:21:51 -0400

    second lab

---
+++ Adder/src/Adder.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author karenr.romero8
+ */
+import java.util.Scanner;
+public class Adder {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner in;
+        in = new Scanner(System.in);
+        int x,y,sum;
+        System.out.println("Give me the first integer to add:");
+           x = input.nextInt(); 
+        System.out.println("Give me the second integer to add:");
+           y = input.nextInt();
+           sum = x+y;
+           System.out.println("The Total number is:");
+           System.out.println(sum);
+           
+    }
+}
